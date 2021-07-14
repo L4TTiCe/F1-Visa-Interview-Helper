b5b9de5b68db200fd5c8f7f769c39d082ccd4ec9
Dheshan Mohandass <dheshan.mail@gmail.com>
2021-07-14 17:32:07 +0530

    initla commit
    Signed-off-by: Dheshan Mohandass <dheshan.mail@gmail.com>

---
+++ README.md
@@ -0,0 +1,10 @@
+## Visa Prep Helper
+
+This simple python script helps you prepare for your upcoming F-1 Visa Interview, by standing in for your VO.
+
+### How to Use
+
+1. Run the 'driver.py' file in your terminal, using the following command
+   ```
+   python driver.py
+   ```
---
+++ driver.py
@@ -0,0 +1,27 @@
+import json
+import secrets
+
+f = open('db.json',)
+data = json.load(f)
+
+flow = [
+    ["_greeting", "_education", "_finances", "_exit"],
+    ["_greeting", "_finances", "_exit"],
+    ["_greeting", "_aboutNEU", "_finances","_exit"],
+    ["_greeting", "_finances", "_grill", "_exit"],
+    ["_greeting", "_education", "_finances", "_grill", "_exit"],
+    ["_greeting", "_aboutNEU", "_education", "_exit"],
+]
+
+chosen_flow = secrets.choice(flow)
+
+for item in chosen_flow:
+    state = data[item]
+
+    while(state):
+        keys = list(state.keys())
+        chosen_key = secrets.choice(keys)
+        print("VO:"+chosen_key, end="")
+        input("")
+
+        state = state[chosen_key]
