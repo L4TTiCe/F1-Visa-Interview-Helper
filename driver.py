bdd75356286f19752e7707babb98962eb17ac02d
Dheshan Mohandass <dheshan.mail@gmail.com>
2021-07-14 19:28:29 +0530

    Made it loop over on ending * Improved responses and added few new responses
    Signed-off-by: Dheshan Mohandass <dheshan.mail@gmail.com>

--- driver.py
+++ driver.py
@@ -13,15 +13,19 @@ flow = [
     ["_greeting", "_aboutNEU", "_education", "_exit"],
 ]
 
-chosen_flow = secrets.choice(flow)
+while (True):
+    print("----------START----------")
+    chosen_flow = secrets.choice(flow)
 
-for item in chosen_flow:
-    state = data[item]
+    for item in chosen_flow:
+        state = data[item]
 
-    while(state):
-        keys = list(state.keys())
-        chosen_key = secrets.choice(keys)
-        print("VO:"+chosen_key, end="")
-        input("")
+        while(state):
+            keys = list(state.keys())
+            chosen_key = secrets.choice(keys)
+            print("VO: "+chosen_key, end="")
+            input("")
 
-        state = state[chosen_key]
+            state = state[chosen_key]
+    print("-----------END-----------", end="")
+    input("")
